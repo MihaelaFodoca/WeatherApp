b12658a485b7599fab15cd50fadc466fd4e5b65d
MihaFodoca <chis.mihaela@yahoo.com>
2020-09-25 11:04:02 +0300

    First commit

---
+++ JS/WeatherApp.js
@@ -0,0 +1,160 @@
+document.getElementById("display_weather").addEventListener("click", () => {
+    refreshForecast();  
+    displayCurrentWeather();  
+});
+
+document.getElementById("display_forecast").addEventListener("click", () => {
+    loadForecast();
+    displayForecastWeather();
+});
+
+var cityName = document.getElementById("city").value;
+var CurrentWeather = [];
+var ForecastWeather = [];
+
+function displayCurrentWeather() {
+    var cityName = document.getElementById("city").value;
+    var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=" + cityName + "");//definim ce sa faca requestul, nu nitiaza cererea, are 2 param: metoda(get) si url-ul (cu /spre fisier);REST-interactiune cu o resursa
+    xhttp.send();
+    xhttp.onload = function () {
+        if (this.status === 200) {
+            updateCurrentWeather(this.responseText);
+            AddCurrentWeather();
+            CurrentWeather = [];
+            ForecastWeather = [];
+        }
+    }
+}
+
+function AddCurrentWeather() {
+    document.getElementById("description").innerHTML = "Descriere: " + CurrentWeather["weather"][0]["description"];
+    document.getElementById("humidity").innerHTML = "Umiditate: " + CurrentWeather["main"].humidity+"%";
+    document.getElementById("pression").innerHTML = "Presiune: " + CurrentWeather["main"].pressure;
+    document.getElementById("current_temp").innerHTML = "Temperatura curenta: " + CurrentWeather["main"].temp;
+    document.getElementById("minimum").innerHTML = "Minima zilei: " + CurrentWeather["main"].temp_min;
+    document.getElementById("maximum").innerHTML = "Maxima zilei: " + CurrentWeather["main"].temp_max;
+    document.getElementById("weather_icon").innerHTML = "<img src=http://openweathermap.org/img/w/" + CurrentWeather["weather"][0]["icon"] + ".png>";
+}
+
+function updateCurrentWeather(response) {
+    CurrentWeather = JSON.parse(response);
+}
+
+function displayForecastWeather() {
+    var cityName = document.getElementById("city").value;
+    var xhttp = new XMLHttpRequest(); 
+        xhttp.open("GET", "https://api.openweathermap.org/data/2.5/forecast?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=" + cityName + "");//definim ce sa faca requestul, nu nitiaza cererea, are 2 param: metoda(get) si url-ul (cu /spre fisier);REST-interactiune cu o resursa
+    xhttp.send(); 
+    xhttp.onload = function () {
+        if (this.status === 200) {    
+            ForecastWeather = [];
+            updateForecastWeather(this.responseText);  
+        }
+    }
+}
+
+function updateForecastWeather(response) {
+    ForecastWeather = JSON.parse(response);
+    var ForecastListData = ForecastWeather["list"];
+    for (var i = 0; i < ForecastListData.length; i++) {
+        var fullDate = ForecastWeather["list"][i]["dt_txt"];
+        var hour = fullDate.slice(11, 16);
+        var date = fullDate.slice(0, 10);
+        ForecastListData[i].currentDate = date;
+        ForecastListData[i].currentHour = hour;
+    }
+    splitForecastListDataByDay();
+}
+var forecastData = [];  
+
+function splitForecastListDataByDay() {
+    var i = 0;
+    var j = i + 1;
+    var ForecastListData = ForecastWeather["list"];
+
+    for (i = 0; i < ForecastListData.length; i++) {
+        index1 = 0;
+        index2 = 0;
+        for (i = 0; i < ForecastListData.length; i++) {
+            if (ForecastListData[i]["currentDate"] == ForecastListData[0]["currentDate"]) {
+                index1 += 1;
+                var day1 = ForecastListData.slice(0, index1);
+                index2 = index1 + 1;
+            }
+        }
+        
+        for (j = index1 + 1; j < ForecastListData.length; j++) {
+            if (ForecastListData[j]["currentDate"] == ForecastListData[index1 + 1]["currentDate"]) {
+                index2 += 1;
+                var day2 = ForecastListData.slice(index1, index2);
+                index3 = index2 + 1
+            }
+        }
+        
+        for (k = index2 + 1; k < ForecastListData.length; k++) {
+            if (ForecastListData[k]["currentDate"] == ForecastListData[index2 + 1]["currentDate"]) {
+                index3 += 1;
+                var day3 = ForecastListData.slice(index2, index3);
+                index4 = index3 + 1
+            }
+        }
+        for (l = index4; l < ForecastListData.length; l++) {
+            if (ForecastListData[l]["currentDate"] == ForecastListData[index3 + 1]["currentDate"]) {
+                index4 += 1;
+                var day4 = ForecastListData.slice(index3, index4);
+                index5 = index4 + 1
+            }
+        }
+        for (m = index5; m < ForecastListData.length; m++) {
+            if (ForecastListData[m]["currentDate"] == ForecastListData[index4 + 1]["currentDate"]) {
+                index5 += 1;
+                var day5 = ForecastListData.slice(index4, index5);
+                index6 = index5 + 1
+            }
+        }
+        for (n = index6; n < ForecastListData.length; n++) {
+            if (ForecastListData[n]["currentDate"] == ForecastListData[index5 + 1]["currentDate"]) {
+                index6 += 1;
+                var day6 = ForecastListData.slice(index5, index6);
+            }
+        }
+    }
+    document.getElementById("day1_date").innerHTML = "Ziua: " + day1[0]["currentDate"];
+    document.getElementById("day2_date").innerHTML = "Ziua: " + day2[0]["currentDate"];
+    document.getElementById("day3_date").innerHTML = "Ziua: " + day3[0]["currentDate"];
+    document.getElementById("day4_date").innerHTML = "Ziua: " + day4[0]["currentDate"];
+    document.getElementById("day5_date").innerHTML = "Ziua: " + day5[0]["currentDate"];
+    document.getElementById("day6_date").innerHTML = "Ziua: " + day6[0]["currentDate"];
+    var i
+    for (i = 0; i < day1.length; i++) {
+        document.getElementById("day1_1").innerHTML += "<br>" + "<img src=http://openweathermap.org/img/w/" + day1[i]["weather"][0]["icon"] + ".png>" + "<br>" + "Ora: " + day1[i]["currentHour"] + "<br>" + "Temperatura: " + day1[i]["main"]["temp"] + "<br>" + "Descriere: " + day1[i]["weather"][0]["description"];
+    }
+    for (i = 0; i < day2.length; i++) {
+        document.getElementById("day2_1").innerHTML += "<br>" + "<img src=http://openweathermap.org/img/w/" + day2[i]["weather"][0]["icon"] + ".png>" + "<br>" + "Ora: " + day2[i]["currentHour"] + "<br>" + "Temperatura: " + day2[i]["main"]["temp"] + "<br>" + "Descriere: " + day2[i]["weather"][0]["description"];
+    }
+    for (i = 0; i < day3.length; i++) {
+        document.getElementById("day3_1").innerHTML += "<br>" + "<img src=http://openweathermap.org/img/w/" + day3[i]["weather"][0]["icon"] + ".png>" + "<br>" + "Ora: " + day3[i]["currentHour"] + "<br>" + "Temperatura: " + day3[i]["main"]["temp"] + "<br>" + "Descriere: " + day3[i]["weather"][0]["description"];
+    }
+    for (i = 0; i < day4.length; i++) {
+        document.getElementById("day4_1").innerHTML += "<br>" + "<img src=http://openweathermap.org/img/w/" + day4[i]["weather"][0]["icon"] + ".png>" + "<br>" + "Ora: " + day4[i]["currentHour"] + "<br>" + "Temperatura: " + day4[i]["main"]["temp"] + "<br>" + "Descriere: " + day4[i]["weather"][0]["description"];
+    }
+    for (i = 0; i < day5.length; i++) {
+        document.getElementById("day5_1").innerHTML += "<br>" + "<img src=http://openweathermap.org/img/w/" + day5[i]["weather"][0]["icon"] + ".png>" + "<br>" + "Ora: " + day5[i]["currentHour"] + "<br>" + "Temperatura: " + day5[i]["main"]["temp"] + "<br>" + "Descriere: " + day5[i]["weather"][0]["description"];
+    }
+    for (i = 0; i < day6.length; i++) {
+        document.getElementById("day6_1").innerHTML += "<br>" + "<img src=http://openweathermap.org/img/w/" + day6[i]["weather"][0]["icon"] + ".png>" + "<br>" + "Ora: " + day6[i]["currentHour"] + "<br>" + "Temperatura: " + day6[i]["main"]["temp"] + "<br>" + "Descriere: " + day6[i]["weather"][0]["description"];
+    }
+}
+function refreshForecast(){
+    document.getElementById("day1_1").innerHTML = "";
+    document.getElementById("day2_1").innerHTML = "";
+    document.getElementById("day3_1").innerHTML = "";
+    document.getElementById("day4_1").innerHTML = "";
+    document.getElementById("day5_1").innerHTML = "";
+    document.getElementById("day6_1").innerHTML = "";
+    document.getElementById("forecast").style.display="none";
+}
+function loadForecast(){
+    document.getElementById("forecast").style.display="block";
+}
